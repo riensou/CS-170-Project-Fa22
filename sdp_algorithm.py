503ac5f0a797b51a3053e5c123a7ad1e8165af7a
Ryan Campbell <ryancampbell@berkeley.edu>
2022-11-18 18:23:13 -0800

    trying to add stuff but its hard

--- sdp_algorithm.py
+++ sdp_algorithm.py
@@ -1,5 +1,12 @@
+from starter import *
+import random
+import cvxpy as cp
+import numpy as np
+
+
 def sdp_max_k_cut_solve(k):
-    def sdp_helper():
+
+    def sdp_helper(G: nx.Graph):
         
 
     return sdp_helper
