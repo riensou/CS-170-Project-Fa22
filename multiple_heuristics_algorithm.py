ff838338ad3c827c5cd172af5b0f30c82632e0b3
Ryan Campbell <ryancampbell@berkeley.edu>
2022-11-27 21:45:57 -0800

    organization

--- multiple_heuristics_algorithm.py
+++
@@ -1,3 +0,0 @@
-# The following algorithm is based on the paper: 
-# "A Multiple Search Operator Heuristic for the Max-k-cut Problem", by Fuda Ma and Jin-Kao Hao
-
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -9,18 +9,19 @@ from starter import *
 in_dir = "inputs"
 out_dir = "outputs"
 
-def etl(file):
+def simulated_annealing(file, overwrite=False):
+
+    start_t = time.perf_counter()
 
     in_file = str(Path(in_dir) / file)
     out_file = str(Path(out_dir) / f"{file[:-len('.in')]}.out")
 
     initial_score = best_score = score(read_output(read_input(in_file), out_file))
 
-    start_t = time.perf_counter()
-
     G = read_output(read_input(in_file), out_file)
 
-    for _ in range(5):
+    # Implement efficient simulated annealing algorithm
+    for _ in range(10):
         output = [G.nodes[v]['team'] for v in range(G.number_of_nodes())]
         teams, counts = np.unique(output, return_counts=True)
         k = np.max(teams)
@@ -38,21 +39,47 @@ def etl(file):
             best_score = current_score
         else:
             G.nodes[most_impact]['team'] = output[most_impact]
+    # Implement efficient simulated annealing algorithm
+
+
+    if overwrite and initial_score > best_score:
+        write_output(G, out_file, overwrite=True)
 
     end_t = time.perf_counter()
 
-    return initial_score - best_score, end_t - start_t
+    return in_file, initial_score - best_score, end_t - start_t
+
+
+
+# These operators are based off of the following:
+# "A Multiple Search Operator Heuristic for the Max-k-cut Problem", by Fuda Ma and Jin-Kao Hao
+
+def O1_operator(G: nx.Graph):
+    """Applies the single-transfer move operation such that the induced move gain is maximum."""
+    # TODO
+    pass
+
+def O2_operator(G: nx.Graph):
+    """Applies the double-transfer move operation such that the induced move gain is maximum."""
+    # TODO
+    pass
+
+# TODO: O3_operator
+# TODO: O4_operator
+
+def O5_operator(G: nx.Graph):
+    """Assigns a random vertex to a random team such that the new team is different than the current team."""
+    # TODO
+    pass
+
+
+
 if __name__ == '__main__':
+
     filenames = [x for x in os.listdir(in_dir) if x.endswith('.in')]
 
     with Pool() as pool:
-        results = pool.imap_unordered(etl, filenames)
+        results = pool.imap_unordered(simulated_annealing, filenames)
 
-        for improvement, duration in results:
-            print(f"improved by {improvement:.2f} in {duration:.2f}s")
-
-
-    # if initial_score > best_score:
-    #     print("I would write this")
-    #     #write_output(G, out_file, overwrite=True)
-    # print(f"{str(in_file)}: cost", score(instance))
+        for filename, improvement, duration in results:
+            print(f"{filename}: improved by {improvement:.2f} in {duration:.2f}s")
