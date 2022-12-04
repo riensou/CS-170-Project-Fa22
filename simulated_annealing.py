74b6e45165b17922fe5f3b34a322f268dab3245f
Ryan Campbell <ryancampbell@berkeley.edu>
2022-12-04 00:38:59 -0800

    wrote a lot of the MOH algo
    Co-authored-by: atdwfd <atdwfd@users.noreply.github.com>

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -310,7 +310,7 @@ def O2_operator(bucket):
     b_norm = np.linalg.norm(b, 2)
     n = G.number_of_nodes()
 
-    for move in tqdm(moves):
+    for move in moves:
         current_output = output.copy()
         current_bucket = copy.deepcopy(bucket)
 
@@ -373,43 +373,127 @@ def O2_operator(bucket):
 
     min_gain = min(potential_sequences.keys())
 
-    print(min_gain)
-    print(random.choice(potential_sequences[min_gain]))
+    #print(min_gain)
+    #print(random.choice(potential_sequences[min_gain]))
 
     return random.choice(potential_sequences[min_gain]), min_gain
 
-
-
-    # This seems hard to implement efficiently, but the paper gives some good advice on how we can do this.
-    # (if we can't figure out a better way) we could have a fixed number of moves to search and pick the best of those moves
-
-    pass
-
-def O3_operator(G: nx.Graph):
+def O3_operator(bucket, H):
     """
     Selects the best single-transfer move, like 01, that is not in the tabu list, H.
     Returns the move and the induced move gain, as well as appending the move to the tabu list H.
     """
-    # TODO
-    pass
+    def min_key(x):
+        moves = []
+        for v in bucket.buckets[x[0]][x[1]]: # is a list of vertices
+            moves.append([v, x[0] + 1])
 
-def O4_operator(G: nx.Graph):
+        if any([move not in H for move in moves]):
+            return x[1]
+        else:
+            return float('inf')
+
+    best_bucket = min([[b, min(bucket.buckets[b])] for b in range(bucket.k)], key=min_key)
+
+    #move = [random.choice(bucket.buckets[best_bucket[0]][best_bucket[1]]), best_bucket[0] + 1]
+
+    move = random.choice([[v, best_bucket[0] + 1] for v in bucket.buckets[best_bucket[0]][best_bucket[1]] if [v, best_bucket[0] + 1] not in H])
+
+    gain = best_bucket[1]
+
+    return move, gain
+
+def O4_operator(bucket):
     """
     Selects the two best single-transfer moves for vertices going to randomly selected teams a and b.
     Returns the move and the induced move gain.
     """
-    # TODO
-    pass
 
-def O5_operator(G: nx.Graph):
+    def min_key(team):
+        def min_key_helper(x):
+            if x[0] + 1 == team:
+                return x[1]
+            else:
+                return float('inf')
+        return min_key_helper
+        
+        
+    
+    G = bucket.G
+    potential_sequences = {}
+
+    output = [G.nodes[v]['team'] for v in range(G.number_of_nodes())]
+    teams, counts = np.unique(output, return_counts=True)
+    k = np.max(teams)
+    two_go_two = random.sample(range(1, k + 1), 2)
+    #print("two_go_two:", two_go_two) #test
+    moves = []
+    for i in (G.nodes):
+        for j in range(1, k + 1):
+            if output[i] != j and (j in two_go_two):
+                moves.append([i, j])
+    
+    C_w_1, C_k_1, C_b_1 = score(G, separated=True)
+
+    b = (counts / G.number_of_nodes()) - 1 / k
+    b_norm = np.linalg.norm(b, 2)
+    n = G.number_of_nodes()
+
+    for move in moves:
+        current_output = output.copy()
+        current_bucket = copy.deepcopy(bucket)
+
+        C_w_partial = sum(d * helper(current_output[i], current_output[j], move[1]) for i, j, d in current_bucket.G.edges(move[0], data='weight') if i == move[0])
+                
+        inside_sqrt = (b_norm ** 2) - (b[current_output[move[0]] - 1] ** 2) - (b[move[1] - 1] ** 2) + ((b[current_output[move[0]] - 1] - 1 / n) ** 2) + ((b[move[1] - 1] + 1 / n) ** 2)
+        C_b_change = math.exp(B_EXP * np.sqrt(inside_sqrt)) - C_b_1
+        gain1 = round(C_w_partial + C_b_change, 10)
+        
+        current_bucket.update(move)
+
+        if move[1] == two_go_two[0]:
+            other_team = two_go_two[1]
+        else:
+            other_team = two_go_two[0]
+        best_bucket = min([[b, min(bucket.buckets[b])] for b in range(bucket.k)], key=min_key(other_team))
+        next_move = [random.choice(bucket.buckets[best_bucket[0]][best_bucket[1]]), best_bucket[0] + 1]
+        gain2 = best_bucket[1]
+
+
+        total_gain = round(gain1 + gain2, 10)
+
+        if total_gain in list(potential_sequences.keys()):
+            potential_sequences[total_gain].append([move, next_move])
+        else:
+            potential_sequences[total_gain] = [[move, next_move]]
+
+    
+
+    min_gain = min(potential_sequences.keys())
+
+    moves = random.choice(potential_sequences[min_gain])
+
+    #print(min_gain)
+    #print(moves)
+
+    return moves, min_gain
+
+def O5_operator(bucket):
     """
     Selects an assignment of a random vertex to a random team such that the new team is different than the current team.
     Returns the move and the induced move gain.
     """
-    # TODO
-    pass
+    G = bucket.G
+
+    v = random.sample(G.nodes, 1)
+    team = random.sample([i for i in range(1, bucket.k + 1) if i != G.nodes[v]['team']], 1)
+
+    return [v, team]
 
 def MOH_algorithm(file, overwrite=True):
+    OMEGA = 100
+    RHO = 0.5
+    #XI = 80
 
     start_t = time.perf_counter()
 
@@ -424,6 +508,8 @@ def MOH_algorithm(file, overwrite=True):
 
     ### MOH algorithm ###
 
+    bucket = Bucket_Structure(I)
+
     I_best = I.copy() 
     local_optimum_score = initial_score
     best_score = initial_score
@@ -431,44 +517,72 @@ def MOH_algorithm(file, overwrite=True):
     iter = 0
 
     # while not stop condition # here we can set a time limit for the algorithm to run depending on graph size or something
+    for _ in range(5):
         
         ## Descent-based improvement phase (O1 and O2) ##
         # while I can be improved by O1 and O2
-
+        moveO1, gainO1 = O1_operator(bucket)
+        movesO2, gainO2 = O2_operator(bucket)
+        while gainO1 < 0 or gainO2 < 0:
             # while O1 improves I
-                # do O1 and update bucket data structure (what is this? read the paper)
+            while gainO1 < 0:
+                I.nodes[moveO1[0]]['team'] = moveO1[1]
+                bucket.update(moveO1)
+                moveO1, gainO1 = O1_operator(bucket)
+                # do O1 and update bucket data structure 
                 # iter += 1
+                iter += 1
 
+            movesO2, gainO2 = O2_operator(bucket)
             # while O2 improves I
-                # do O2 and update bucket data structure (what is this? read the paper)
+            while gainO2 < 0:
+                for move in movesO2:
+                    I.nodes[move[0]]['team'] = move[1]
+                    bucket.update(move)
+                movesO2, gainO2 = O2_operator(bucket)
+                # do O2 and update bucket data structure
                 # iter += 1
-        ##
-        
-        # current_score = score(I)
-        # local_optimum_score = current_score
-        # if current_score < best_score
-            # best_score = current_score
-            # I_best = I.copy()
-            # c_non_impv = 0
-        # else
-            # c_non_impv += 1
+                iter += 1
+        ## Out of phase 1 (O1 and O2)
+
+        current_score = score(I)
+        local_optimum_score = current_score
+        if current_score < best_score:
+            best_score = current_score
+            I_best = I.copy()
+            c_non_impv = 0
+        else:
+            c_non_impv += 1
 
         ## Diversified improvement phase (O3 and O4) ##
-        # c_div = 0
 
-        # while c_div <= lower_omega and score(I) > local_optimum_score
+        c_div = 0
+        H = []
 
-            # if random(0, 1) < lower_rho:
+        while c_div <= OMEGA and current_score >= local_optimum_score:
+            LAMBDA = random.randint(3, int(I.number_of_nodes() / 10))
+
+            if random.uniform(0, 1) < RHO:
+                for tabu in H:
+                    if tabu[2] <= iter:
+                        H.remove(tabu)
+                move, gain = O3_operator(bucket, [m[0:2] for m in H])
+                H.append([move[0], I.nodes[move[0]]['team'], iter + LAMBDA])
                 # apply O3 on I
-            # else:
-                # apply O4 on I
-            # Update bucket data structure and H
-            # iter += 1
-            # c_div += 1
+                bucket.update(move)
+            else:
+                moves, gain = O4_operator(bucket)
+                for move in moves:
+                    I.nodes[move[0]]['team'] = move[1]
+                    bucket.update(move)
+            iter += 1
+            c_div += 1
+
+            current_score += gain
         ##
 
         ## Perturbation phase (O5) ##
-        # if c_non_impv > lower_xi
+        #if c_non_impv > XI:
             # apply O5 (gamma times)
             # c_non_impv
         ##
@@ -490,7 +604,7 @@ if __name__ == '__main__':
     filenames = [x for x in os.listdir(in_dir) if x.endswith('.in')]
 
     with Pool() as pool:
-        results = pool.imap_unordered(the_number_one_algorithm, filenames)
+        results = pool.imap_unordered(MOH_algorithm, filenames)
 
         for filename, improvement, duration in results:
             print(f"{filename}: improved by {improvement:.2f} in {duration:.2f}s")
